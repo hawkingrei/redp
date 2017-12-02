201e8ce9f478da5e53e7e00ad3253c78dcc8225e
真人 <weizhen.wang@duitang.com>
2017-12-02 22:40:55 +0800

    fix: test

--- cmd/test_test.go
+++ cmd/test_test.go
@@ -12,6 +12,8 @@ func TestAllHeader(t *testing.T) {
 	var conf conf.Configure
 	conf.DbDriver = "mysql"
 	conf.DbURL = "root:@/redp?charset=utf8&parseTime=True&loc=Local"
+	conf.Debug = true
+	conf.HBtimeout = 3
 	store_ := CreateStote(&conf)
 	handler := CreateHttpHandler(store_, &conf)
 
@@ -30,7 +32,7 @@ func TestAllHeader(t *testing.T) {
 	handler.ServeHTTP(w, r)
 
 	w = httptest.NewRecorder()
-	r, _ = http.NewRequest("POST", "/api/user", nil)
+	r, _ = http.NewRequest("POST", "/api/hongbao", nil)
 	r.Header.Add("Signature", "wz:d0965c07d1a00fcc85d28b8a241aa")
 	r.Header.Add("money", "10")
 	r.Header.Add("num", "1")
