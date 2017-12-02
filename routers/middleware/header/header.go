c6332d13a5650c37f0a10bc5b7373a60a008f452
真人 <weizhen.wang@duitang.com>
2017-12-02 19:04:28 +0800

    feat: update routers

--- routers/middleware/header/header.go
+++ routers/middleware/header/header.go
@@ -3,7 +3,6 @@ package header
 import (
 	"net/http"
 	"time"
-	"github.com/hawkingrei/redp/internal/version"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,13 +11,5 @@ func NoCache(c *gin.Context) {
 	c.Header("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate, value")
 	c.Header("Expires", "Thu, 01 Jan 1970 00:00:00 GMT")
 	c.Header("Last-Modified", time.Now().UTC().Format(http.TimeFormat))
-	c.Next()
-}
-
-// Version is a middleware function that appends the Redp
-// version information to the HTTP response. This is intended
-// for debugging and troubleshooting.
-func Version(c *gin.Context) {
-	c.Header("X-REDP-VERSION", version.GitCommit)
 	c.Next()
 }
--- routers/middleware/version.go
+++ routers/middleware/version.go
@@ -6,8 +6,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Version is a middleware function that appends the Drone version information
-// to the HTTP response. This is intended for debugging and troubleshooting.
+// Version is a middleware function that appends the Redp
+// version information to the HTTP response. This is intended
+// for debugging and troubleshooting.
 func Version(c *gin.Context) {
 	c.Header("X-REDP-VERSION", version.GitCommit)
+	c.Next()
 }
