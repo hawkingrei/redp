0ab3aa6fc4d8110e7afde38a9cc53a8b1b9113fb
真人 <weizhen.wang@duitang.com>
2017-12-02 23:05:36 +0800

    feat: update test

--- cmd/test_test.go
+++ cmd/test_test.go
@@ -20,24 +20,34 @@ func TestSimpleApi(t *testing.T) {
 	w := httptest.NewRecorder()
 	r, _ := http.NewRequest("GET", "/api/version", nil)
 	handler.ServeHTTP(w, r)
+	if r.Response.StatusCode != 200 {
+		t.Error("GET /api/version not 200")
+	}
 
 	w = httptest.NewRecorder()
 	r, _ = http.NewRequest("GET", "/api/user", nil)
 	r.Header.Add("Signature", "wz:d0965c07d1a00fcc85d28b8a241ae35a")
 	handler.ServeHTTP(w, r)
+	if r.Response.StatusCode != 200 {
+		t.Error("GET /api/user not 200")
+	}
 
 	w = httptest.NewRecorder()
 	r, _ = http.NewRequest("GET", "/api/user", nil)
 	r.Header.Add("Signature", "wz:d0965c07d1a00fcc85d28b8a241aa")
 	handler.ServeHTTP(w, r)
-
+	if r.Response.StatusCode != 200 {
+		t.Error("GET /api/user not 200")
+	}
 	w = httptest.NewRecorder()
 	r, _ = http.NewRequest("POST", "/api/hongbao", nil)
 	r.Header.Add("Signature", "wz:d0965c07d1a00fcc85d28b8a241aa")
 	r.Header.Add("money", "10")
 	r.Header.Add("num", "1")
 	handler.ServeHTTP(w, r)
-
+	if r.Response.StatusCode != 200 {
+		t.Error("POST /api/hongbao not 200")
+	}
 	w = httptest.NewRecorder()
 	r, _ = http.NewRequest("GET", "/api/hongbao", nil)
 	r.Header.Add("Signature", "wz:d0965c07d1a00fcc85d28b8a241aa")
