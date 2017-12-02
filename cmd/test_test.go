e6d2a9e6d505b85e3d169037896b90b0293dbd97
真人 <weizhen.wang@duitang.com>
2017-12-02 22:37:11 +0800

    feat: update router

--- cmd/test_test.go
+++ cmd/test_test.go
@@ -14,15 +14,26 @@ func TestAllHeader(t *testing.T) {
 	conf.DbURL = "root:@/redp?charset=utf8&parseTime=True&loc=Local"
 	store_ := CreateStote(&conf)
 	handler := CreateHttpHandler(store_, &conf)
+
 	w := httptest.NewRecorder()
 	r, _ := http.NewRequest("GET", "/api/version", nil)
 	handler.ServeHTTP(w, r)
+
 	w = httptest.NewRecorder()
 	r, _ = http.NewRequest("GET", "/api/user", nil)
 	r.Header.Add("Signature", "wz:d0965c07d1a00fcc85d28b8a241ae35a")
 	handler.ServeHTTP(w, r)
+
 	w = httptest.NewRecorder()
 	r, _ = http.NewRequest("GET", "/api/user", nil)
 	r.Header.Add("Signature", "wz:d0965c07d1a00fcc85d28b8a241aa")
 	handler.ServeHTTP(w, r)
+
+	w = httptest.NewRecorder()
+	r, _ = http.NewRequest("POST", "/api/user", nil)
+	r.Header.Add("Signature", "wz:d0965c07d1a00fcc85d28b8a241aa")
+	r.Header.Add("money", "10")
+	r.Header.Add("num", "1")
+	handler.ServeHTTP(w, r)
+
 }
