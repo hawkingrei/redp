02f01ad8505c354ca04cddbac4fbc455fff3730a
真人 <weizhen.wang@duitang.com>
2017-12-02 17:47:46 +0800

    feat: update cli

--- cmd/redp-cli.go
+++ cmd/redp-cli.go
@@ -47,23 +47,7 @@ func loadmeta(configFile string) (meta *conf.Configure, err error) {
 	return
 }
 
-func main() {
-	flagSet := redpFlagSet()
-	flagSet.Parse(os.Args[1:])
-
-	if flagSet.Lookup("version").Value.(flag.Getter).Get().(bool) {
-		fmt.Println(version.String())
-		os.Exit(0)
-	}
-	configFile := flagSet.Lookup("config").Value.String()
-	config, err := loadmeta(configFile)
-
-	if config.Debug {
-		logrus.SetLevel(logrus.DebugLevel)
-	} else {
-		logrus.SetLevel(logrus.WarnLevel)
-	}
-
+func CreateStote(config *conf.Configure) store.Store {
 	store_, err := setupStore(config)
 	if err != nil {
 		logrus.Error(err.Error())
@@ -72,21 +56,45 @@ func main() {
 	store_.CreateTable(&model.User{})
 	store_.CreateTable(&model.SendedHongbao{})
 	store_.CreateTable(&model.GotHongbao{})
+	return store_
+}
 
-	var g errgroup.Group
-	quit := make(chan os.Signal)
+func CreateHttpHandler(store store.Store, config *conf.Configure) http.Handler {
 	handler := routers.Load(
 		ginrus.Ginrus(logrus.StandardLogger(), time.RFC3339, true),
 		middleware.Version,
-		middleware.Store(config, store_),
+		middleware.Store(config, store),
 	)
+	return handler
+}
 
+func main() {
+	flagSet := redpFlagSet()
+	flagSet.Parse(os.Args[1:])
+	if flagSet.Lookup("version").Value.(flag.Getter).Get().(bool) || len(os.Args) == 1 {
+		fmt.Println(version.String())
+		os.Exit(0)
+	}
+	configFile := flagSet.Lookup("config").Value.String()
+	config, err := loadmeta(configFile)
+	if err != nil {
+		logrus.Error(err.Error())
+		os.Exit(0)
+	}
+	if config.Debug {
+		logrus.SetLevel(logrus.DebugLevel)
+	} else {
+		logrus.SetLevel(logrus.WarnLevel)
+	}
+	quit := make(chan os.Signal)
+	store_ := CreateStote(config)
+	handler := CreateHttpHandler(store_, config)
 	signal.Notify(quit, os.Interrupt)
 	serve := &http.Server{
 		Addr:    ":9000",
 		Handler: handler,
 	}
-
+	var g errgroup.Group
 	g.Go(func() error {
 		return serve.ListenAndServe()
 	})
