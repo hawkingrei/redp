a98a3c41310d6a3c2d4aaa8d566b000be6d4e523
真人 <weizhen.wang@duitang.com>
2017-12-02 23:09:20 +0800

    feat: update test

--- cmd/test_test.go
+++ cmd/test_test.go
@@ -49,6 +49,14 @@ func TestSimpleApi(t *testing.T) {
 		t.Error("POST /api/hongbao not 200")
 	}
 	w = httptest.NewRecorder()
+	r, _ = http.NewRequest("GET", "/api/hongbao/1", nil)
+	r.Header.Add("Signature", "wz:d0965c07d1a00fcc85d28b8a241aa")
+	handler.ServeHTTP(w, r)
+	if r.Response.StatusCode != 200 {
+		t.Error("GET /api/hongbao not 200")
+	}
+
+	w = httptest.NewRecorder()
 	r, _ = http.NewRequest("GET", "/api/hongbao", nil)
 	r.Header.Add("Signature", "wz:d0965c07d1a00fcc85d28b8a241aa")
 	handler.ServeHTTP(w, r)
