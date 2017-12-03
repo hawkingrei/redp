f860409713aa2fafc0d173b6491d8e7ecc414dbf
真人 <weizhen.wang@duitang.com>
2017-12-03 22:23:16 +0800

    fix: error columns

--- model/user.go
+++ model/user.go
@@ -3,5 +3,5 @@ package model
 type User struct {
 	Uid      int64   `gorm:"AUTO_INCREMENT;primary_key"`
 	Username string  `gorm:"not null;unique;unique_index"`
-	Memory   float32 `sql:"type:decimal(10,2);"`
+	Money    float32 `sql:"type:decimal(10,2);"`
 }
