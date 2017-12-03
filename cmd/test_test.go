51b234b4ec6d4a56fd01b690bde1c57b9d6ee77d
真人 <weizhen.wang@duitang.com>
2017-12-03 15:35:38 +0800

    test: fix

--- cmd/test_test.go
+++ cmd/test_test.go
@@ -84,7 +84,7 @@ func TestSimpleApi(t *testing.T) {
 			"Password":  password,
 		}).
 		Run(handler, func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
-			assert.NotEqual(t, http.StatusOK, r.Code, r.Body.String())
+			assert.Equal(t, http.StatusOK, r.Code, r.Body.String())
 		})
 	r.GET("/api/hongbao").
 		SetDebug(true).
